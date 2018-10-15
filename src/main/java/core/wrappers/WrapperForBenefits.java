1785deca387345d91ab536a20579138bbd1bd224
daniil <pavlov.danya@yandex.ru>
2018-10-15 18:09:51 +0300

    Extract contants xpath selector in WrapperForBenefits

--- src/main/java/core/wrappers/WrapperForBenefits.java
+++ src/main/java/core/wrappers/WrapperForBenefits.java
@@ -6,13 +6,13 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-import java.util.HashMap;
-
 import static org.junit.Assert.assertTrue;
 
 public class WrapperForBenefits {
 
 
+    private static final By ICON_IN_BENEFITS = By.xpath(".//div[@class='benefit-icon']");
+    private static final By TEXT_IN_BENEFITS = By.xpath(".//span[@class='benefit-txt']");
     private static CheckerForBenefits checkerForBenefits = new CheckerForBenefits();
     private WebElement element;
     private WebDriver driver;
@@ -25,17 +25,17 @@ public class WrapperForBenefits {
 
 
     public WrapperForBenefits checkBenefitIconDisplayed() {
-        assertTrue("One of icon's isn't displayed!", element.findElement(By.xpath(".//div[@class='benefit-icon']")).isDisplayed());
+        assertTrue("One of icon's isn't displayed!", element.findElement(ICON_IN_BENEFITS).isDisplayed());
         return this;
     }
 
     public WrapperForBenefits checkBenefitTextDisplayed() {
-        assertTrue("One of text's for benefits isn't displayed!", element.findElement(By.xpath(".//span[@class='benefit-txt']")).isDisplayed());
+        assertTrue("One of text's for benefits isn't displayed!", element.findElement(TEXT_IN_BENEFITS).isDisplayed());
         return this;
     }
 
     public WrapperForBenefits checkBenefitTextRight() {
-        checkerForBenefits.checkElementText(element.findElement(By.xpath(".//span[@class='benefit-txt']")));
+        checkerForBenefits.checkElementText(element.findElement(TEXT_IN_BENEFITS));
         return this;
     }
     public static void checkAllTextsChecked() {
